33ab6d910113c23b0ceb28a643c68c81824c9991
Jesús de la Fuente <jfuentelucas@gmail.com>
2025-03-30 00:24:57 +0100

    footer changes

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -9,17 +9,17 @@ const Footer = () => {
   return (
     <footer className='bg-stone-500 text-white font-bold text-center px-10 py-5 border-t-4 border-red-500 mt-20 '>
       <h2 className="text-xl font-bold">Flink-Pizza & Co.</h2>
-      <div className="flex flex-col text-xl">
+      <div className="flex flex-col text-xl mb-5">
         {isOpen ? (
           <p> We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
           online.</p>
         ) : (
-          <p>
+          <p >
             We're happy to welcome you between {openHour}:00 and {closeHour}:00.
           </p>
         )}
       </div>
-      <div className=" mt-6 pt-4 text-center  text-white text-lg">
+      <div className="pt-4 text-center border-t border-white text-white text-lg">
         &copy; {year} delafuentej. This in a project as part of The Ultimate React Course 2025 from Jonas Schmedt.
       </div>
 
