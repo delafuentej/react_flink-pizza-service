6d844a26a798495c62164835200e6cd6c67b1729
Jesús de la Fuente <jfuentelucas@gmail.com>
2025-03-30 00:19:50 +0100

    footer

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,9 +1,29 @@
 
 
 const Footer = () => {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  const year = new Date().getFullYear();
   return (
-    <footer className='bg-stone-500 text-white font-bold text-center px-4 py-2 border-t-4 border-red-500'>
-        Footer
+    <footer className='bg-stone-500 text-white font-bold text-center px-10 py-5 border-t-4 border-red-500 mt-20 '>
+      <h2 className="text-xl font-bold">Flink-Pizza & Co.</h2>
+      <div className="flex flex-col text-xl">
+        {isOpen ? (
+          <p> We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+          online.</p>
+        ) : (
+          <p>
+            We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+          </p>
+        )}
+      </div>
+      <div className=" mt-6 pt-4 text-center  text-white text-lg">
+        &copy; {year} delafuentej. This in a project as part of The Ultimate React Course 2025 from Jonas Schmedt.
+      </div>
+
+      
     </footer>
   )
 }
